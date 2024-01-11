3216f2a454bf56e69788ad37d603ea1664c571dd
Zul Ham Fahlevi <zulham.fahlevi.73@gmail.com>
2024-01-11 15:41:29 +0700

    fix grid col card anime and margin top

--- src/modules/populer/index.tsx
+++ src/modules/populer/index.tsx
@@ -6,7 +6,7 @@ import AnimListModule from '../anime-list';
 
 const PopulerModule = ({ data, page }: { data: TAnime; page: number }) => {
   return (
-    <div className="p-5">
+    <div className="my-5">
       <HeaderMenu title={`ANIME TERPOPULER #${page}`} />
       <AnimListModule animeList={data} />
       <Pagination dataPagination={data.pagination} />
